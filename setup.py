22c943ea98e61be6e54bd2115b708b8260c2f4e8
Joe Mader <jmader@jmader.com>
2020-09-30 09:46:53 -0600

    baseline

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+import madeira
+
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='madeira',
+    version=madeira.__version__,
+    description='Common AWS operation wrappers',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mxmader/madeira",
+    author='Joe Mader',
+    author_email='jmader@jmader.com',
+    packages=find_packages(exclude=['*.tests', '*.tests.*']),
+    install_requires=[
+        'boto3',
+        'psycopg2-binary'
+    ]
+)
