6117f977f0e927577efa3e3be06eb7e627ee94f2
Joe Mader <jmader@jmader.com>
2020-09-30 09:58:32 -0600

    cleanup

--- madeira/route_53.py
+++ madeira/route_53.py
@@ -1,5 +1,6 @@
 import madeira
 import boto3
+import uuid
 
 
 class Route_53_Wrapper(object):
@@ -9,18 +10,20 @@ class Route_53_Wrapper(object):
         self._session = boto3.session.Session(profile_name=profile_name, region_name=region)
         self._r53_client = self._session.client('route53')
 
-    def create_hosted_zone(self, dns_domain):
+    def create_hosted_zone(self, dns_domain, caller_reference=None):
+        caller_reference = caller_reference if caller_reference else uuid.uuid4().hex
         self._logger.debug('Creating hosted zone for DNS domain: %s', dns_domain)
-        self._r53_client.
-        
+        self._r53_client.create_hosted_zone(
+            Name=dns_domain, CallerReference=caller_reference)
 
     def get_hosted_zone_id(self, dns_domain):
         self._logger.debug('Looking up hosted zone ID for domain: %s', dns_domain)
         for hosted_zone in self._r53_client.list_hosted_zones().get('HostedZones'):
             # Hosted zones have trailing dots...
-            if hosted_zone['Name'] == '{}.'.format(dns_domain):
+            if hosted_zone['Name'] == f'{dns_domain}':
                 return hosted_zone['Id']
         self._logger.debug('No hosted zone found')
 
     def get_domain_ns_records(self, hosted_zone_id):
-        return self._r53_client.get_hosted_zone(Id=hosted_zone_id)['DelegationSet']['NameServers']
+        hosted_zone = self._r53_client.get_hosted_zone(Id=hosted_zone_id)
+        return hosted_zone['DelegationSet']['NameServers']
